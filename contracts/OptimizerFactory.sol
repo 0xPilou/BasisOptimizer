3de5c01c340a69fc89e769e8c705010d46728847
PierreLvl <pierre.louvel@protonmail.com>
2021-03-21 01:20:28 +0800

    add addProtocol function

--- contracts/OptimizerFactory.sol
+++ contracts/OptimizerFactory.sol
@@ -4,12 +4,12 @@ pragma solidity >=0.7.0 <0.8.0;
 
 import "./Optimizer.sol";
 
-contract OptimizerFactory {
+contract OptimizerFactory is Ownable {
     address[] public optimizers;
     Protocol[] public protocols;
 
     struct Protocol {
-        string name;
+        uint256 protocolId;
         address LP;
         address share;
         address dollar;
@@ -39,6 +39,24 @@ contract OptimizerFactory {
         for (uint i = 0; i < ownerOptimizers.length; i++) {
             Optimizer(ownerOptimizers[i]).harvest();
         }
+    }
+
+    function addProtocol(
+        address _LP,
+        address _share,
+        address _dollar,
+        address _shareRewardPool,
+        address _boardroom
+    ) external onlyOwner returns(uint256){
+        Protocol memory newProtocol;
+        newProtocol.protocolId = protocols.length;
+        newProtocol.LP = _LP;
+        newProtocol.share = _share;
+        newProtocol.dollar = _dollar;
+        newProtocol.shareRewardPool = _shareRewardPool;
+        newProtocol.boardroom = _boardroom;
+        protocols.push(newProtocol);
+        return newProtocol.protocolId;
     }
 
 }
