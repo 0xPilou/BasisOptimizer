b72f0549682820abead634678d9c330f465c28e7
PierreLvl <pierre.louvel@protonmail.com>
2021-03-22 23:30:55 +0800

    progress on Optimizer tests

--- test/1_test_Optimizer.js
+++ test/1_test_Optimizer.js
@@ -1,11 +1,11 @@
-/**
- *  Mock Contracts
- */
+
 const LP = artifacts.require("LP");
 const Share = artifacts.require("Share");
 const Dollar = artifacts.require("Dollar");
 const ShareRewardPoolMock = artifacts.require("ShareRewardPoolMock");
 const BoardroomMock = artifacts.require("BoardroomMock");
+const Web3 = require("web3");
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
 
 /**
  *  Contract Under Tests
@@ -13,33 +13,83 @@ const BoardroomMock = artifacts.require("BoardroomMock");
 const Optimizer = artifacts.require("Optimizer");
 
 
-contract("Optimizer Unit Tests", async (deployer, network, [owner])=> {
-    it("should deposit LP to the Share Reward Pool ", async ([owner]) => {
-        const optimizer = (await Optimizer.deployed());
-        const lp = (await LP.deployed());
-        const shareRewardPool = (await ShareRewardPoolMock.deployed());
-        const amountToDeposit = await lp.balanceOf(owner);
+contract("Optimizer Unit Tests", () => {
+    let optimizer = null;
+    let lp = null;
+    let share = null;
+    let shareRewardPool = null;
+    let accounts = null;
+    before(async () => {
+        accounts = web3.eth.getAccounts().then(function(acc){ accounts = acc })
+        optimizer = await Optimizer.deployed();
+        lp = await LP.deployed();
+        share = await Share.deployed();
+        shareRewardPool = await ShareRewardPoolMock.deployed();
+    });
+    it("should deposit 100 LP token to the Share Reward Pool", async () => {
+        const amountToDeposit = web3.utils.toWei('100', 'ether');
+        await lp.approve(optimizer.address, amountToDeposit);
         await optimizer.depositLP(amountToDeposit);
-
-        assert.equal(lp.balanceOf(owner), 0);   
-        assert.equal(lp.balanceOf(shareRewardPool), amountToDeposit);   
-
-
+        const poolBal = await lp.balanceOf(shareRewardPool.address);      
+        assert.equal(poolBal, amountToDeposit);   
     });
-    it("should withdraw LP from the Share Reward Pool", async () => {
+  //  it("should not be able to withdraw more LP than the amount deposited", async () => {
+  //      const amountToWithdraw = 100;
+  //      await optimizer.withdrawLP(amountToWithdraw);
+  //      
+  //  });
+    it("should withdraw 100 LP token from the Share Reward Pool", async () => {
+        const amountToWithdraw = web3.utils.toWei('100', 'ether');
 
+        const userLpBal_before = web3.utils.fromWei(await lp.balanceOf(accounts[0]));
+        const optimizerShareBal_before = web3.utils.fromWei(await share.balanceOf(optimizer.address));
+        const poolShareBal_before = web3.utils.fromWei(await share.balanceOf(shareRewardPool.address));
+
+        await optimizer.withdrawLP(amountToWithdraw);
+
+        const userLpBal_after = web3.utils.fromWei(await lp.balanceOf(accounts[0]));
+        const optimizerShareBal_after = web3.utils.fromWei(await share.balanceOf(optimizer.address));
+        const poolShareBal_after = web3.utils.fromWei(await share.balanceOf(shareRewardPool.address));
+
+        assert.equal(userLpBal_after - userLpBal_before, 100);
+        assert.equal(optimizerShareBal_after - optimizerShareBal_before, poolShareBal_before - poolShareBal_after);
     });
-
     it("should deposit Share to the Boardroom", async () => {
-
+        const amountToDeposit = web3.utils.toWei('2000', 'ether');
+        await share.approve(optimizer.address, amountToDeposit);
+        await optimizer.depositShare(amountToDeposit);
+        const boardRoomBal = await share.balanceOf(BoardroomMock.address);      
+        assert.equal(boardRoomBal, amountToDeposit);           
     });
 
-    it("should withdraw Share from the Boardroom", async () => {
+  // it("should withdraw Share from the Boardroom", async () => {
 
-    });
+  // });
 
-    it("should ...", async () => {
+  // it("should ...", async () => {
 
-    });
+  // });
 
 });
+
+
+
+//console.log(
+//    `====================================
+//    User Balances Before :
+//        > LP : ${userLpBal_before}
+//    User Balances After :
+//        > LP : ${userLpBal_after}
+//    =======================================
+//    Share Reward Pool Balances Before :
+//        > Share : ${poolShareBal_before}
+//    Share Reward Pool Balances After :
+//        > Share : ${poolShareBal_after}
+//    =======================================
+//    Optimizer Balances Before :
+//        > Share : ${optimizerShareBal_before}
+//    Optimizer Balances After :
+//        > Share : ${optimizerShareBal_after}
+//    =======================================
+//    `
+//);
