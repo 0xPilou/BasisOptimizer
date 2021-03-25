efbae2c4123444ab8c70648237daf689ad0efdce
PierreLvl <pierre.louvel@protonmail.com>
2021-03-25 23:41:27 +0800

    rework OptimizerFactory

--- test/2_test_OptimizerFactory.js
+++ test/2_test_OptimizerFactory.js
@@ -1,27 +1,46 @@
 /**
+*  Dependencies
+*/
+const Web3 = require("web3");
+const truffleAssert = require('truffle-assertions');
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+
+/**
  *  Mock Contracts
  */
- const LP = artifacts.require("LP");
- const Share = artifacts.require("Share");
- const Dollar = artifacts.require("Dollar");
- const ShareRewardPoolMock = artifacts.require("ShareRewardPoolMock");
- const BoardroomMock = artifacts.require("BoardroomMock");
- 
- /**
-  *  Contract Under Tests
-  */
- const Optimizer = artifacts.require("Optimizer");
- const OptimizerFactory = artifacts.require("OptimizerFactory");
+const LP = artifacts.require("LP");
+const Share = artifacts.require("Share");
+const Dollar = artifacts.require("Dollar");
+const ShareRewardPoolMock = artifacts.require("ShareRewardPoolMock");
+const BoardroomMock = artifacts.require("BoardroomMock");
 
+/**
+ *  Contract Under Tests
+ */
+const Optimizer = artifacts.require("Optimizer");
+const OptimizerFactory = artifacts.require("OptimizerFactory");
 
-contract("Optimizer Factory Unit Tests", async (deployer, network, accounts)=> {
+contract("OptimizerFactory Unit Tests", () => {
+    let accounts = null;
+    let optimizer = null;
+    let lp = null;
+    let share = null;
+    let dollar = null;
+    let shareRewardPool = null;
+    let boardroom = null;
+    let optimizerFactory = null;
+    
+    before(async () => {
+        accounts = web3.eth.getAccounts().then(function(acc){ accounts = acc })
+        lp = await LP.deployed();
+        share = await Share.deployed();
+        dollar = await Dollar.deployed();
+        shareRewardPool = await ShareRewardPoolMock.deployed();
+        boardroom = await BoardroomMock.deployed();
+        optimizerFactory = await OptimizerFactory.deployed();
+
+    });
     it("should add a new protocol to the Optimizer Factory ", async () => {
-        let optimizerFactory = (await OptimizerFactory.deployed());
-        const lp = (await LP.deployed());
-        const share = (await Share.deployed());
-        const dollar = (await Dollar.deployed());
-        const boardroom = (await BoardroomMock.deployed());
-        const shareRewardPool = (await ShareRewardPoolMock.deployed());
         await optimizerFactory.addProtocol(lp.address, share.address, dollar.address, shareRewardPool.address, boardroom.address);
         const result = await optimizerFactory.protocols(0);
         assert.equal(result.protocolId, 0);   
@@ -32,45 +51,32 @@ contract("Optimizer Factory Unit Tests", async (deployer, network, accounts)=> {
         assert.equal(result.boardroom, boardroom.address);
     });
     it("should get the number of protocols supported", async () => {
-        let optimizerFactory = (await OptimizerFactory.deployed());
-        const lp = (await LP.deployed());
-        const share = (await Share.deployed());
-        const dollar = (await Dollar.deployed());
-        const boardroom = (await BoardroomMock.deployed());
-        const shareRewardPool = (await ShareRewardPoolMock.deployed());
         await optimizerFactory.addProtocol(lp.address, share.address, dollar.address, shareRewardPool.address, boardroom.address);
-        const result = await optimizerFactory.getProtocolCount();
-        assert.equal(result.toNumber(), 2);
+        const result = (await optimizerFactory.getProtocolCount()).toNumber();
+        assert.equal(result, 2);
+    });
+    it("should get the number of optimizers created (0)", async () => {
+        const result = (await optimizerFactory.getOptimizerCount()).toNumber();
+        assert.equal(result, 0);
     });
     it("should create a new optimizer with Protocol ID 2", async () => {
-        let optimizerFactory = (await OptimizerFactory.deployed());
-        const lp = (await LP.deployed());
-        const share = (await Share.deployed());
-        const dollar = (await Dollar.deployed());
-        const boardroom = (await BoardroomMock.deployed());
-        const shareRewardPool = (await ShareRewardPoolMock.deployed());
         await optimizerFactory.addProtocol(lp.address, share.address, dollar.address, shareRewardPool.address, boardroom.address);
-        await optimizerFactory.createOptimizer(2)
-        const result = await optimizerFactory.optimizers(0);
-        assert.equal(optimizerFactory.optimizers(0), optimizerFactory.optimizers(0));   
+        await optimizerFactory.createOptimizer(2);
+        const newOptimizerId = (await optimizerFactory.getOptimizerCount()).toNumber(); 
+        const newOptimizerAddr = await optimizerFactory.optimizers(newOptimizerId - 1);
+        console.log(newOptimizerAddr);
+        optimizer = await Optimizer.at(newOptimizerAddr);
+
+        await lp.approve(optimizer.address, 100);
+        await optimizer.depositLP(100);
+        const result = await optimizer.LpStaked;     
+        console.log(result);
     });
-    it("should get the number of optimizers created ", async () => {
-        let optimizerFactory = (await OptimizerFactory.deployed());
-        const lp = (await LP.deployed());
-        const share = (await Share.deployed());
-        const dollar = (await Dollar.deployed());
-        const boardroom = (await BoardroomMock.deployed());
-        const shareRewardPool = (await ShareRewardPoolMock.deployed());
-        await optimizerFactory.addProtocol(lp.address, share.address, dollar.address, shareRewardPool.address, boardroom.address);
-        await optimizerFactory.createOptimizer(1)
-        const result = await optimizerFactory.getOptimizerCount(0);
-        assert.equal(result.toNumber(), 2);
-    });
-
-
-
-
-
-
+  //  it("should get the number of optimizers created ", async () => {
+  //      await optimizerFactory.addProtocol(lp.address, share.address, dollar.address, shareRewardPool.address, boardroom.address);
+  //      await optimizerFactory.createOptimizer(1)
+  //      const result = await optimizerFactory.getOptimizerCount(0);
+  //      assert.equal(result.toNumber(), 2);
+  //  });
 });
    
